f3661179092f8ccafdc0f5f5fb3ce4ef835c7b9f
Olivia Letendart <letendartolivia@gmail.com>
2020-06-11 21:09:50 +0200

    create components contains footer + integration in the file app.vue + modificate indentation in the file body.vue

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App" />
     <Body />
+    <Footer />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
-    Body
+    Body,
+    Footer
   }
 };
 </script>
--- src/components/Body.vue
+++ src/components/Body.vue
@@ -1,13 +1,138 @@
 <template>
-  <div>
+   <div>
+    <h4>Vidéo récentes</h4>
+
     <b-card>
-        <b-embed
-        type="iframe"
-        aspect="4by3"
-        src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0">
-        </b-embed>
-    </b-card>
-  </div>
+        <b-media no-body>
+
+        <b-media-body class="ml-3">
+            <h5 class="mt-0">Titre de la vidéo</h5>
+        
+
+            <b-media>
+                <b-embed
+                type="iframe"
+                aspect="16by9"
+                src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+                class="w-50 h-50">
+                </b-embed>
+            </b-media>
+        </b-media-body>
+
+        <b-media-body class="ml-3">
+            <h5 class="mt-0">Titre de la vidéo</h5>
+        
+
+            <b-media>
+            <b-embed
+                type="iframe"
+                aspect="16by9"
+                src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+                class="w-50 h-50">
+            </b-embed>
+            </b-media>
+        </b-media-body>
+        </b-media>
+        <b-media no-body>
+
+        <b-media-body class="ml-3">
+            <h5 class="mt-0">Titre de la vidéo</h5>
+        
+
+            <b-media>
+                <b-embed
+                type="iframe"
+                aspect="16by9"
+                src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+                class="w-50 h-50">
+                </b-embed>
+            </b-media>
+        </b-media-body>
+
+        <b-media-body class="ml-3">
+            <h5 class="mt-0">Titre de la vidéo</h5>
+        
+
+            <b-media>
+                <b-embed
+                type="iframe"
+                aspect="16by9"
+                src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+                class="w-50 h-50">
+                </b-embed>
+            </b-media>
+        </b-media-body>
+        </b-media>
+
+</b-card>
+
+<h4>Vos préférences</h4>
+
+  <b-card>
+    <b-media no-body>
+
+      <b-media-body class="ml-3">
+        <h5 class="mt-0">Titre de la vidéo</h5>
+        
+
+        <b-media>
+            <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+            class="w-50 h-50">
+            </b-embed>
+        </b-media>
+      </b-media-body>
+
+      <b-media-body class="ml-3">
+        <h5 class="mt-0">Titre de la vidéo</h5>
+        
+
+        <b-media>
+            <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+            class="w-50 h-50">
+            </b-embed>
+        </b-media>
+      </b-media-body>
+    </b-media>
+    <b-media no-body>
+
+      <b-media-body class="ml-3">
+        <h5 class="mt-0">Titre de la vidéo</h5>
+        
+
+        <b-media>
+            <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+            class="w-50 h-50">
+            </b-embed>
+        </b-media>
+      </b-media-body>
+
+      <b-media-body class="ml-3">
+        <h5 class="mt-0">Titre de la vidéo</h5>
+        
+
+        <b-media>
+            <b-embed
+            type="iframe"
+            aspect="16by9"
+            src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+            class="w-50 h-50">
+            </b-embed>
+        </b-media>
+      </b-media-body>
+    </b-media>
+
+</b-card>    
+  
+</div>
 </template>
 
 <script>
@@ -20,9 +145,4 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-b-embed {
-    height: 100px;
-    wight: 100px;
-}
-</style>
+<style scoped></style>
