dae8c188da03a91c0461eeb1d1158a103c65ab27
Olivia Letendart <letendartolivia@gmail.com>
2020-06-11 20:24:30 +0200

    create components body contains differents components + modificate app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Body />
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import Body from "./components/Body";
 
 export default {
   name: "App",
   components: {
     HelloWorld,
+    Body
   }
 };
 </script>
---
+++ src/components/Body.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <b-card>
+        <b-embed
+        type="iframe"
+        aspect="4by3"
+        src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0">
+        </b-embed>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Body",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+b-embed {
+    height: 100px;
+    wight: 100px;
+}
+</style>
